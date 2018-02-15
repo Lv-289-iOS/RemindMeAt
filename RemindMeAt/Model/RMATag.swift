f975f04a2a55c2c86f77c2ac5e7f2594f83c4f58
Artem Reznikov <temarez@gmail.com>
2018-02-16 00:24:30 +0200

    Implement convenience init for RMATag and RMARealmManager.seedData()

--- RemindMeAt/Model/RMATag.swift
+++ RemindMeAt/Model/RMATag.swift
@@ -13,4 +13,10 @@ class RMATag: Object {
     @objc dynamic var name = ""
     @objc dynamic var color: String = UIColor.clear.hexString()
     
+    convenience init(tagName: String, tagColor: UIColor) {
+        self.init() // Please note this says 'self' and not 'super'
+        self.name = tagName
+        self.color = tagColor.hexString()
+    }
+    
 }
