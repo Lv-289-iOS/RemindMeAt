d4f17f71451981bf937d725e0f5fd0648230d3fa
Artem Reznikov <temarez@gmail.com>
2018-02-16 12:05:52 +0200

    Add UIColor.fromHexString() and simplify UIColor.hexString() in order not to use alpha channel

--- RemindMeAt/Model/UIColorExtension.swift
+++ RemindMeAt/Model/UIColorExtension.swift
@@ -9,12 +9,8 @@
 import UIKit
 
 @objc extension UIColor {
-    /**
-     Hex string of a UIColor instance, fails to empty string.
-     
-     - parameter includeAlpha: Whether the alpha should be included.
-     */
-    public func hexString(_ includeAlpha: Bool = true) -> String  {
+    /** Hex string of a UIColor instance, fails to empty string. */
+    public func hexString() -> String  {
         var r: CGFloat = 0
         var g: CGFloat = 0
         var b: CGFloat = 0
@@ -25,11 +21,33 @@ import UIKit
             return ""
         }
         
-        if (includeAlpha) {
-            return String(format: "#%02X%02X%02X%02X", Int(r * 255), Int(g * 255), Int(b * 255), Int(a * 255))
-        } else {
-            return String(format: "#%02X%02X%02X", Int(r * 255), Int(g * 255), Int(b * 255))
+        return String(format: "#%02X%02X%02X", Int(r * 255), Int(g * 255), Int(b * 255))
+    }
+    
+    /**
+     The UIColor from Hex string, fails to UIColor.clear
+     - parameter hexString: representation of color in String HEX form #RRGGBB
+     */
+    public static func fromHexString(_ hexString: String) -> UIColor {
+        var result = UIColor.clear
+        guard hexString.hasPrefix("#") else {
+            return result
         }
+        
+        let hexString: String = String(hexString[String.Index.init(encodedOffset: 1)...])
+        var hexValue:  UInt32 = 0
+        
+        guard Scanner(string: hexString).scanHexInt32(&hexValue) else {
+            return result
+        }
+        
+        let divisor = CGFloat(255)
+        let red     = CGFloat((hexValue & 0xFF0000) >> 16) / divisor
+        let green   = CGFloat((hexValue & 0x00FF00) >>  8) / divisor
+        let blue    = CGFloat( hexValue & 0x0000FF       ) / divisor
+        result = UIColor(red: red, green: green, blue: blue, alpha: 1)
+
+        return result
     }
     
 }
