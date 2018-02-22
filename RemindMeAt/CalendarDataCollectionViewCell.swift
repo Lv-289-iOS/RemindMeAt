b1d727a83c5e06c31b12f81bf71c449c04958583
Володимир Смульський <smulia18ua@gmail.com>
2018-02-22 16:22:12 +0200

    Add Calendar Controller(not complieted)

---
+++ RemindMeAt/CalendarDataCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CalendarDataCollectionViewCell.swift
+//  RemindMeAt
+//
+//  Created by Володимир Смульський on 2/22/18.
+//  Copyright © 2018 SoftServe Academy. All rights reserved.
+//
+
+import UIKit
+
+class CalendarDataCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var DateLable: UILabel!
+    
+}
---
+++ RemindMeAt/CalendarsVars.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarsVars.swift
+//  RemindMeAt
+//
+//  Created by Володимир Смульський on 2/22/18.
+//  Copyright © 2018 SoftServe Academy. All rights reserved.
+//
+
+import Foundation
+
+let date = Date()
+let calendar = Calendar.current
+
+let day = calendar.component(.day, from: date)
+let weekday = calendar.component(.weekday,from: date )
+var month = calendar.component(.month, from: date)
+var year = calendar.component(.year, from: date)
