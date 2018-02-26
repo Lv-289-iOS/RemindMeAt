b9bd463570dc390f21f977773ed70892714da70e
Yurii Tsymbala <zumbala2013@gmail.com>
2018-02-26 12:39:10 +0200

    Added periodicity controller

---
+++ RemindMeAt/Controllers/RMAPeriodicityViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RMAPeriodicityViewController.swift
+//  RemindMeAt
+//
+//  Created by Yurii Tsymbala on 2/26/18.
+//  Copyright © 2018 SoftServe Academy. All rights reserved.
+//
+
+import UIKit
+
+class RMAPeriodicityViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBOutlet weak var pickerView: UIPickerView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
